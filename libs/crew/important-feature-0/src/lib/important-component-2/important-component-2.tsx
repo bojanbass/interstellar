bc135ae5b20b00e697359a80bd68a7c196762ce3
Bojan Bratuz <bojanbass@gmail.com>
2022-02-28 23:07:28 +0100

    test

--- libs/crew/important-feature-0/src/lib/important-component-2/important-component-2.tsx
+++ libs/crew/important-feature-0/src/lib/important-component-2/important-component-2.tsx
@@ -1,9 +1,8 @@
-
-  import * as components from '@interstellar/shared/components'
-  import * as alerts from '@interstellar/shared/alerts'
-  import * as dialogs from '@interstellar/shared/dialogs'
-  import * as icons from '@interstellar/shared/icons'
-  import * as buttons from '@interstellar/shared/buttons'
+import * as components from '@interstellar/shared/components';
+import * as alerts from '@interstellar/shared/alerts';
+import * as dialogs from '@interstellar/shared/dialogs';
+import * as icons from '@interstellar/shared/icons';
+import * as buttons from '@interstellar/shared/buttons';
 import './important-component-2.module.css';
 
 /* eslint-disable-next-line */
@@ -12,7 +11,7 @@ export interface ImportantComponent2Props {}
 export function ImportantComponent2(props: ImportantComponent2Props) {
   return (
     <div>
-      <h1>Welcome to important-component-2 a ss!</h1>
+      <h1>Welcome to important-component-2 a ss fas!</h1>
     </div>
   );
 }
